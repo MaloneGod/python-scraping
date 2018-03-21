2cef43ad52e77910f7547e7fd1249206cb3517c4
MaloneGod <809829352@qq.com>
2018-03-21 20:20:58 +0800

    Update 2-fileSubmission.py

--- chapter9/2-fileSubmission.py
+++ chapter9/2-fileSubmission.py
@@ -1,6 +1,6 @@
 import requests
 
-files = {'uploadFile': open('../files/Python-logo.png', 'rb')}
+files = {'image': open('../files/Python-logo.png', 'rb')}
 r = requests.post("http://pythonscraping.com/pages/processing2.php", 
                   files=files)
 print(r.text)
